1fcb4f94686f7d1e83f5392c8f085ca2dfec9f8e
rodry <rodrymza@gmail.com>
2025-10-23 02:56:05 -0300

    Adaptacion a AuthResponse, con retorno de datos de usuario y token

--- final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/service/AuthServiceImpl.java
+++ final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/service/AuthServiceImpl.java
@@ -1,61 +1,52 @@
 package com.backend.finalprog3.spring.service;
 
-import com.backend.finalprog3.spring.entity.Rol;
+import com.backend.finalprog3.spring.dto.AuthResponse;
+import com.backend.finalprog3.spring.dto.LoginRequestDTO;
+import com.backend.finalprog3.spring.dto.RegistroRequestDTO;
+import com.backend.finalprog3.spring.dto.UsuarioDTO;
 import com.backend.finalprog3.spring.entity.Usuario;
+import com.backend.finalprog3.spring.mapper.UsuarioMapper;
 import com.backend.finalprog3.spring.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioMapper usuarioMapper;
+    @Autowired
+    private UsuarioService usuarioService;
 
     @Override
-    public Usuario register(Usuario usuario) {
+    public AuthResponse register(RegistroRequestDTO usuarioRegistro) {
 
-        //Chequea si el mail existe:
+        UsuarioDTO usuarioCreado = usuarioService.crearUsuario(usuarioRegistro);
 
-        if(usuarioRepository.findByEmail(usuario.getEmail()).isPresent()){
-            throw new RuntimeException("El mail ingresado ya existe. Intente con otro");
-        }
+        //Creacion de token de autenticacion
+        String token = "token_simulado_aqui";
 
-        //Si no se asigna ningun rol que por defecto sea un rol = usuario;
-        if(usuario.getRol() == null){
-            usuario.setRol(Rol.USUARIO);
-        }
-
-        //Hasheo de clave:
-
-        usuario.setPassword(Sha256Util.hash(usuario.getPassword()));
-
-
-        //Guardamos y retornamos:
-
-        return usuarioRepository.save(usuario);
+        return new AuthResponse(token, usuarioCreado);
     }
 
     @Override
-    public Usuario login(String email, String password) {
+    public AuthResponse login(LoginRequestDTO usuarioLogin) {
 
-        //Busca usuarios por mail:
-
-        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(() ->
-                new RuntimeException("Usuario o email invalido"));
+        Usuario usuario = usuarioRepository.findByEmail(usuarioLogin.email()).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Email invalido"));
 
         //Se hashea la clave del login para poder compararlas:
+        String contrasenaHasheada = Sha256Util.hash(usuarioLogin.password());
 
-        String contrasenaHasheada = Sha256Util.hash(password);
-
-        if(!usuario.getPassword().equals(contrasenaHasheada)){
-            throw new RuntimeException("Contraseña o email incorrecto");
+        if(!usuario.getContrasena().equals(contrasenaHasheada)){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Contraseña incorrecta");
         }
+        String token =  "token_simulado_aqui";
 
-        //Para no devolver la clave
-
-        usuario.setPassword(null);
-
-        return usuario;
+        return new AuthResponse(token, usuarioMapper.toDto(usuario));
     }
 }
