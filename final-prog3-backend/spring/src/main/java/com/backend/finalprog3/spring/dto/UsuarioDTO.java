ef544c1e2f94550c66e1ac75f095e5fde700cacd
rodry <rodrymza@gmail.com>
2025-10-23 02:58:17 -0300

    creacion UsuarioDTO

---
+++ final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/dto/UsuarioDTO.java
@@ -0,0 +1,13 @@
+package com.backend.finalprog3.spring.dto;
+
+import com.backend.finalprog3.spring.entity.Rol;
+
+public record UsuarioDTO(
+        Long id,
+        String nombre,
+        String apellido,
+        String email,
+        String celular,
+        Rol rol,
+        boolean activo) {
+}
