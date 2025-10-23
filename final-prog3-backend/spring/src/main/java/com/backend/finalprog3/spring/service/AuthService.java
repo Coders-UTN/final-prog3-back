e001f196cd8b33a4424a654c6c9a83322cfd891c
rodry <rodrymza@gmail.com>
2025-10-23 03:09:26 -0300

    eliminada notacion public

--- final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/service/AuthService.java
+++ final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/service/AuthService.java
@@ -5,8 +5,8 @@ import com.backend.finalprog3.spring.dto.LoginRequestDTO;
 import com.backend.finalprog3.spring.dto.RegistroRequestDTO;
 
 public interface AuthService {
-    public AuthResponse register(RegistroRequestDTO usuario);
-    public AuthResponse login(LoginRequestDTO usuarioLogin);
+    AuthResponse register(RegistroRequestDTO usuario);
+    AuthResponse login(LoginRequestDTO usuarioLogin);
 
 
 }
