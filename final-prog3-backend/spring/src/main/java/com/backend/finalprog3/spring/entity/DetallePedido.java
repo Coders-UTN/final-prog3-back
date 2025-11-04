7cbfd89ed7f8e22245a4fffff14926fffb93c749
rodry <rodrymza@gmail.com>
2025-11-04 19:16:52 -0300

    Creacion de entidades para pedido

---
+++ final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/entity/DetallePedido.java
@@ -0,0 +1,36 @@
+package com.backend.finalprog3.spring.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int cantidad;
+
+    @Column(nullable = false)
+    private double precioUnitario;
+
+    @Column(nullable = false)
+    private double subtotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+}
