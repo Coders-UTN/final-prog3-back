4929ea5bfbf8339e5e14b7a97111c18653cd1dab
rodry <rodrymza@gmail.com>
2025-11-09 16:59:43 -0300

    modificaciones para evitar fallos con cors

--- final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/controller/UsuarioController.java
+++ final-prog3-backend/spring/src/main/java/com/backend/finalprog3/spring/controller/UsuarioController.java
@@ -7,13 +7,11 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuario")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
